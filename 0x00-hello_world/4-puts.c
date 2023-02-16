549b829151ee42356446d51e221b263364bc1c67
Kehali Tsehay <kehalits@gmail.com>
2023-02-16 07:25:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * This is awesome c programing language
+ * The return value is 0, which is good.
+ *
+ * */
 int main(void)
 {
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
