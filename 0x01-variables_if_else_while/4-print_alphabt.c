ec233e1add45fb733fa54fb2ec93f9d0f45253b1
Kehali Tsehay <kehalits@gmail.com>
2023-02-17 01:08:38 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+
+	while (c ,= 'z')
+	{
+		if ((c != 'q' && c != 'e') && (c <= 'z'))
+		{
+			putcahr(c);
+			c++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
