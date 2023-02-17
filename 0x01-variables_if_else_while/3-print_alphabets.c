27ab94b918a839905260cbe3850e9da6a1167738
Kehali Tsehay <kehalits@gmail.com>
2023-02-17 00:54:07 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * return always 0 (succuss)
+ */
+int main(void)
+{
+	char a;
+	char A;
+
+	a = 'a';
+	A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
