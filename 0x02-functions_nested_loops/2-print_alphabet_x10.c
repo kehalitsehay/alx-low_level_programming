bb829990a3345ac6f4626602b9392d21ea89a22b
Kehali Tsehay <kehalits@gmail.com>
2023-02-21 04:04:46 -0800

    Prints numbers in base 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowecase followed by a new line
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+        char num = '0';
+
+        while (num <= '9')
+        {
+                _putchar(num);
+                num++;
+        }
+        _putchar('\n');
+}
