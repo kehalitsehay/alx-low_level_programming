26ada9a08a02345b2ff5a5d5c339b71a320b538b
Kehali Tsehay <kehalits@gmail.com>
2023-02-21 04:50:56 -0800

    alphabets 10 times printed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,19 @@
 
 /**
  * print_alphabet - Entry point
- * Description: a function that prints the alphabet 10 times
+ * Description: a function that prints the alphabet 10 times print_alphabet_x10
  * in lowecase followed by a new line
  * Return: void
  */
 void print_alphabet_x10(void)
 {
+
 	int i;
 	
 	for (i = 0; i <= 9; i++)
 	{
 		char c = 'a';
+
 		while (c <= 'z')
 		{
 			_putchar(c);
