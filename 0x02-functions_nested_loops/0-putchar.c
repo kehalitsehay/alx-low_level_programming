6644a90476a275ee70de6a1d1b48a18dad4b9b75
Kehali Tsehay <kehalits@gmail.com>
2023-02-21 01:34:53 -0800

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-
+#include "alx.h"
+/**
+ * main - Entry point
+ *
+ * Description: This task prints '_putchar\n'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+		_putchar(c);
+		i++;
+	}
+	
+	return (0);
+}
