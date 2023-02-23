4382b8d46f9f9d7a98c544a62ee6e663cbbff4c4
Kehali Tsehay <kehalits@gmail.com>
2023-02-23 01:15:12 -0800

    mutliplication is collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * mul: collaboration is multiplication
+ * @a : is a variable
+ * @b : is an other variable
+ * description: mul returns  products of two number
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
