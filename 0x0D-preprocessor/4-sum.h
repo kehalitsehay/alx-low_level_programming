cdf594cfbbf284d04d28e9a13e43b3ef4fcae1a1
Kehali Tsehay <kehalits@gmail.com>
2023-03-21 05:34:58 -0700

    compute sum of x and y

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,7 +3,9 @@
 
 /*
  * File: 4-sum.h
- * 
+ * SUM: sum of x and y
+ * @x: number 1
+ * @y: number 2
  * Desc: Header file that defines a function-like macro SUM(x, y)
  *       that computes the sum of the numbers x and y.
  */
